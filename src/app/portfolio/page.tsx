6149bb596def0a9e27200448eb4849130f346f99
naufalrafianto <mnaufalrafianto@gmail.com>
2024-10-12 03:07:08 +0700

    fix: add suspense to /portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,7 @@
 import PortfolioPage from '@/components/page/PortfolioList';
 import { getBlogPosts } from '@/lib/blog';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Portfolio',
@@ -9,5 +10,9 @@ export const metadata: Metadata = {
 export default async function Page() {
   const posts = await getBlogPosts();
 
-  return <PortfolioPage initialPosts={posts} />;
+  return (
+    <Suspense fallback={<div>Loading portfolio...</div>}>
+      <PortfolioPage initialPosts={posts} />
+    </Suspense>
+  );
 }
